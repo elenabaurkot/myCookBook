2c9b058af8e0fd05212746b57c22230fd061da8d
elenabaurkot <elenabaurkot@gmail.com>
2020-11-06 23:53:50 -0500

    recipe names and instructions being appended to page upon search, class with sample styling added

--- script.js
+++ script.js
@@ -14,10 +14,24 @@ function getDrinkRecipe() {
       // Stores array of returned drinks in drinks variable
       let drinks = data.drinks;
       drinks.forEach((drink) => {
+        let drinkName = drink.strDrink;
+        let instructions = drink.strInstructions;
+        $('<div>')
+          .addClass('recipeDiv')
+          .append(drinkName, instructions)
+          .appendTo($('#recipe-return'));
+
         console.log(drink);
-        console.log(drink.strDrink);
+        // Get drink name and instructions for making
+        console.log(`Drink name: ${drink.strDrink}`);
+        console.log(`Instructions: ${drink.strInstructions}`);
+        // Get drink ingredients
+        console.log('Drink ingredients: ');
         for (let i = 1; i < 15; i++) {
-          console.log(drink['strIngredient' + i]);
+          let ingredient = drink['strIngredient' + i];
+          if (ingredient != null) {
+            console.log(ingredient);
+          }
         }
       });
     });

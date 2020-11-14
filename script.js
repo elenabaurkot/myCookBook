61a9c0e2a8c00cfa82b1c16dc99811dfdef7ee9d
elenabaurkot <elenabaurkot@gmail.com>
2020-11-13 23:28:05 -0500

    divs dynamically created for recipes, drink name, instructions and ingredients being appended

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Global Variables
 var drinkInput = $('#drink-search');
 var searchButton = $('#search-button');
+let recipeContainer = $('#recipe-return');
 
 // function to get drink user searched for
 function getDrinkRecipe() {
@@ -13,26 +14,28 @@ function getDrinkRecipe() {
     .then((data) => {
       // Stores array of returned drinks in drinks variable
       let drinks = data.drinks;
-      drinks.forEach((drink) => {
-        let drinkName = drink.strDrink;
-        let instructions = drink.strInstructions;
-        $('<div>')
-          .addClass('recipeDiv')
-          .append(drinkName, instructions)
-          .appendTo($('#recipe-return'));
 
-        console.log(drink);
-        // Get drink name and instructions for making
-        console.log(`Drink name: ${drink.strDrink}`);
-        console.log(`Instructions: ${drink.strInstructions}`);
+      drinks.forEach((drink) => {
+        let drinkName = $('<p>').text(drink.strDrink);
+        let instructions = drink.strInstructions;
+        let recipeDiv = $('<div>');
+
+        drinkName.addClass('drinkName');
+
+        recipeDiv
+          .addClass('recipeDiv')
+          .append(drinkName)
+          .append(instructions)
+          .append('Drink Ingredients: ');
+
         // Get drink ingredients
-        console.log('Drink ingredients: ');
         for (let i = 1; i < 15; i++) {
           let ingredient = drink['strIngredient' + i];
           if (ingredient != null) {
-            console.log(ingredient);
+            recipeDiv.append(ingredient);
           }
         }
+        recipeContainer.append(recipeDiv);
       });
     });
 }

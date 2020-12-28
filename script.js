9c3152e9cf2c070b009f80ec05680556bd089556
elenabaurkot <elenabaurkot@gmail.com>
2020-12-27 22:58:42 -0500

    jquery added

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Global Variables
-var userSearch = document.getElementById("drink-search");
-var searchButton = document.getElementById("search-button");
-let recipeContainer = document.getElementById("recipe-return");
+var userSearch = $("#drink-search");
+var searchButton = $("#search-button");
+let recipeContainer = $("#recipe-return");
 
 // function to get drink user searched for
 function getDrinkRecipe(event) {
   event.preventDefault();
 
-  let drinkSearched = userSearch.value;
+  let drinkSearched = userSearch.val();
   let queryURL =
     "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkSearched;
 
@@ -43,20 +43,35 @@ function getDrinkRecipe(event) {
     });
 }
 
-const printRecipeData = function (rTitle, rSteps, rIngred, rImg, rSum) {
+const printRecipeData = function (rTitle, rSteps, rIngred, rImg, rSum, rTime) {
   let recTitle = rTitle;
-  let steps = rSteps.forEach((recipeStep) => console.log(recipeStep.step));
-  let ingredients = rIngred.forEach((ingredient) =>
-    console.log(ingredient.originalString)
-  );
+  let steps = rSteps;
+  let ingredients = rIngred;
   let recipeImage = rImg;
   let summary = rSum;
+  let time = rTime;
+
+  let recipeName = $("<p>");
+  recipeName.text(recTitle).addClass("recipe-name");
+  // recipeContainer.appendChild(recipeName);
+
+  let recipeDiv = $("<div>");
+  recipeDiv.addClass("recipe-div").append(recipeName);
+
+  recipeContainer.append(recipeDiv);
+  // .append(drinkName)
+  // .append(instructions)
+  // .append("Drink Ingredients: ");
+
+  // rSteps.forEach((recipeStep) => console.log(recipeStep.step));
+  // rIngred.forEach((ingredient) => console.log(ingredient.originalString));
 
   console.log(`TITLE = ${recTitle}`);
   console.log(`STEPS = \n ${steps}`);
   console.log(`INGREDIENTS = \n ${ingredients}`);
   console.log(`IMAGEURL = ${recipeImage}`);
   console.log(`SUMMARY = ${summary}`);
+  console.log(`Time = ${time}`);
 };
 
 // Loop through results
@@ -69,8 +84,9 @@ const getRecipeData = function (dataArr) {
     let ingredients = dataArr[i].missedIngredients;
     let imageURL = dataArr[i].image;
     let summary = dataArr[i].summary;
+    let time = dataArr[i].readyInMinutes;
 
-    printRecipeData(title, steps, ingredients, imageURL, summary);
+    printRecipeData(title, steps, ingredients, imageURL, summary, time);
   }
 };
 
@@ -78,19 +94,19 @@ const getRecipes = function (event) {
   event.preventDefault();
 
   // Get user input
-  let searchTerm = userSearch.value;
+  let searchTerm = userSearch.val();
   // console.log(searchTerm);
 
   // query url
-  let queryURL = `https://api.spoonacular.com/recipes/complexSearch?query=${searchTerm}&fillIngredients=true&addRecipeInformation=true&apiKey=fb8f9820c9b74cf1ac411198c8a4e4a0&number=5`;
+  let queryURL = `https://api.spoonacular.com/recipes/complexSearch?query=${searchTerm}&fillIngredients=true&addRecipeInformation=true&apiKey=fb8f9820c9b74cf1ac411198c8a4e4a0&number=50`;
 
   fetch(queryURL)
     .then((response) => response.json())
     .then(({ results }) => {
-      // console.log(results);
+      console.log(results);
       getRecipeData(results);
     });
 };
 
 // Click events
-searchButton.addEventListener("click", getRecipes);
+searchButton.on("click", getRecipes);
